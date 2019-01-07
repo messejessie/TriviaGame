e47c26bc6be109ee43bcd09cbf6906800c58f522
messejessie <messejessie77@gmail.com>
2019-01-06 21:21:41 -0500

    starting point

---
+++ assets/javascript/main.js
@@ -0,0 +1,43 @@
+var myQuestions = [
+    {
+        question: "What are six signs of depresion you should never ignore?",
+        answers: {
+            a: 'Trouble Sleeping, Loss of Interest in Favorite Activities, Increase in Energy, Change in Appetite, Touchiness, An Emerging Dark Side',
+            b: 'Sleeping perfect, Increase Interest in Favorite Activities, Decrease in Energy, Normal Appetite, Tough, A positive attitude',
+            c: 'Jumping, Sudden New Interest, Mild energy, Normal Appetite habits, Not offensive, a balanced attitude',
+            d: 'All of the above'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "What are normal risk factors in Depresison?",
+        answers: {
+            a: 'Personal or family History',
+            b: 'Major life changes, trauma, or stress',
+            c: 'Certain physical illnesses and medications',
+            d: 'All of the above'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "What is Imposter Syndrome?",
+        answers: {
+            a: 'A collection of feelings of superiority that persists despite evident failure',
+            b: 'A collection of feelings that you are a unicorn',
+            c: 'A collection of feelings of inadequacy that persist despite evident success',
+            d: 'All of the above'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "What is Imposter Syndrome?",
+        answers: {
+            a: 'A collection of feelings of superiority that persists despite evident failure',
+            b: 'A collection of feelings that you are a unicorn',
+            c: 'A collection of feelings of inadequacy that persist despite evident success',
+            d: 'All of the above'
+        },
+        correctAnswer: 'd'
+    },
+
+]
